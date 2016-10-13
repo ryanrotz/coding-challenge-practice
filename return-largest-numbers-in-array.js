1c17805284bb59c952c5e5ba7ed66f02dd99ca21
Ryan Rotz <ryanrotz@gmail.com>
2016-10-13 11:32:35 -0700

    sub arrays eliminated. Algorithm finished

--- return-largest-numbers-in-array.js
+++ return-largest-numbers-in-array.js
@@ -4,22 +4,22 @@
 
 // Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 
-
 function largestOfFour(arr) {
-  
+  var arr2 = [];
   for(i=0; i<arr.length; i++) {
     for(j=0; j<arr[i].length+1; j++) {
       if (arr[i][0] <= arr[i][1]) {
         arr[i].shift(arr[i][0]);
       } else if (arr[i][0] > arr[i][1]) {
         arr[i].splice(1,1);
-      }     
+      }
     }
-
+      var num = arr[i][0]
+      arr2.push(num);
+      console.log(arr2)
   }
-  
-  
-  return arr;
+  return arr2;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
